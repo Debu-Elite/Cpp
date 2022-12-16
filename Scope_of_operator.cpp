494a757fcb39edfef78e27bfc31aae049277aeb3
debu <gautamdevansh183@gmail.com>
2022-12-16 16:33:10 +0530

    To access a global variable X when there is a local variable with same name
    Signed-off-by: debu <gautamdevansh183@gmail.com>

---
+++ Scope_of_operator.cpp
@@ -0,0 +1,18 @@
+/*
+To access a global variable when there is a local variable with same name
+to define a function outside a class/ namespace
+To access a class static variables
+in case of multiple inheritance
+
+*/
+#include<iostream>
+using namespace std;
+int x= 100;
+int y= 400;
+int main()
+{
+    int x=10;
+    cout<<"enter the value of x in local"<<x<<endl;
+    cout<<"the value of x in global : "<< ::x<< endl;
+    cout<<"the value of y : "<< y<< endl;
+}
