d621faafc7ad603622c5be229f5c373cbb6507b6
debu <gautamdevansh183@gmail.com>
2022-12-16 16:38:40 +0530

    value of x is not declared globally
    Signed-off-by: debu <gautamdevansh183@gmail.com>

--- Scope_of_operator.cpp
+++ Scope_of_operator.cpp
@@ -7,12 +7,12 @@ in case of multiple inheritance
 */
 #include<iostream>
 using namespace std;
-int x= 100;
+//int x= 100;
 int y= 400;
 int main()
 {
     int x=10;
     cout<<"enter the value of x in local"<<x<<endl;
-    cout<<"the value of x in global : "<< ::x<< endl;
+    cout<<"the value of x in global : "<< ::x<< endl;// x is not decleared globally
     cout<<"the value of y : "<< y<< endl;
 }
