f3138cac3284d15126921d7271c48824e1f63a9f
debu <gautamdevansh183@gmail.com>
2022-12-16 16:12:19 +0530

    basic input/output operation i n C++
    Signed-off-by: debu <gautamdevansh183@gmail.com>

---
+++ input_output_in_cpp.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"enter the value of x"<<endl;
+    cin >> x;
+    cout<<"the value of x is : "<< x<< endl;
+}
---
+++ sum.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"enter the value of x and y"<<endl;
+    cin >> x>>y;
+    cout<<"the value of x+y= : "<< x+y<< endl;
+}
