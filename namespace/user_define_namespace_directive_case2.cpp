4a415730f695705a36ed0aee413da426a6a306ab
debu <gautamdevansh183@gmail.com>
2023-01-04 12:00:13 +0530

    Case 2 error for using namespace directive
    Signed-off-by: debu <gautamdevansh183@gmail.com>

---
+++ namespace/user_define_namespace_directive_case2.cpp
@@ -0,0 +1,36 @@
+/*
+user define namespace using directive method
+*/
+#include<iostream>
+using namespace std;
+namespace SBI
+    {
+        int account_number = 3637;
+        void Sbi_function()
+        {
+            cout<< "welcome to sbi your accout number is: "<< account_number<< endl;
+        }
+    }
+
+
+namespace ICICI
+    {
+        int account_number = 7879;
+        void ICICI_function()
+        {
+            cout<< "welcome to ICICI your accout number is: "<< account_number<< endl;
+        }
+    }
+
+
+//Case 2 error for using namespace directive
+using namespace SBI; // directive method
+using namespace ICICI;
+int main()
+{
+    cout<< account_number<<endl;
+    SBI::Sbi_function();
+    cout<< ICICI :: account_number<<endl;
+    ICICI::ICICI_function();
+
+}
