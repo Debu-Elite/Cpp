70ae1b81fd7b6dec99ae5c418b6275fd9d7120ad
debu <gautamdevansh183@gmail.com>
2023-01-29 20:16:11 +0530

    strings
    Signed-off-by: debu <gautamdevansh183@gmail.com>

---
+++ string.cpp
@@ -0,0 +1,19 @@
+/*
+find the number is even or not
+
+*/
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+string s;
+
+cout<<"enter the string"<<endl;
+getline(cin,s);
+cout<<s<<endl;
+
+
+
+}
