c9e645c6738a37fd1dffe7126c0cefce151c4f3e
debu <gautamdevansh183@gmail.com>
2023-02-06 16:27:04 +0530

    Constructor example
    Signed-off-by: debu <gautamdevansh183@gmail.com>

---
+++ C++_Constructor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class A
+{
+    int x,y;
+public :
+    A()//A is a constructor with the class name
+    {
+        cout<<"constructor"<<endl;
+        x=10;
+        y=20;
+    }
+
+    void get_data()
+    {
+        cout<<"x="<<x<<"\n"<<"y ="<<y<<endl;
+    }
+
+};
+int main()
+{
+  A a1;
+  a1.get_data();
+}
